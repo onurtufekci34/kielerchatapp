824096b2d111d22cd474b376796f6c955ee80454
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-17 17:23:52 +0100

    users messages

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { db } from "../firebase/config"
-import { collection,query,where,getDocs } from "firebase/firestore"
+import { collection,query,where,getDocs,getDoc,doc,updateDoc,serverTimestamp, setDoc } from "firebase/firestore"
+import {AuthContext} from '../contexts/AuthContext'
 
 
 
@@ -11,6 +12,8 @@ export default function Search() {
   const [searchUser, setSearchUser]= useState("")
   const [user , setUser] = useState(null)
   const [error,setError] = useState(false)
+
+  const {loginUser} =useContext(AuthContext)
 
   const handleSearch = async ()=> {
     const q = query(collection(db,"users"),where("userName","==",searchUser))
@@ -23,7 +26,7 @@ export default function Search() {
         setUser(doc.data())
       })
     } catch (error) {
-      setError(error)
+      setError(true)
     }
 
 
@@ -37,7 +40,43 @@ export default function Search() {
 
 
 
+const handleSelect = async ()=>{
+    const combinedId = loginUser.uid>user.uid ? loginUser.uid +user.uid : user.uid + loginUser.uid;
 
+
+    try {
+      
+      const res = await getDoc(doc(db,"chats", combinedId))
+
+      if(!res.exists())
+
+        await setDoc(doc(db,"chats",combinedId),{messages:[]})
+
+        await updateDoc(doc(db,"userChats", loginUser.uid),{
+          [combinedId + ".userInformation"]:{
+            uid:user.uid,
+            userName:user.userName,
+            photoURL:user.photoURL
+          },
+          [combinedId + ".date"]:serverTimestamp()
+        });
+
+        await updateDoc(doc(db,"userChats", user.uid),{
+          [combinedId + ".userInformation"]:{
+            uid:loginUser.uid,
+            userName:loginUser.displayName,
+            photoURL:loginUser.photoURL
+          },
+          [combinedId + ".date"]:serverTimestamp()
+        });
+
+    } catch (error) {
+      
+    }
+
+    setUser(null)
+    setSearchUser("")
+}
 
 
   return (
@@ -46,9 +85,9 @@ export default function Search() {
         <input type="text" placeholder="Member Search"  onKeyDown={handleKey} onChange={(e)=> setSearchUser(e.target.value)} value={searchUser}/>
       </div>
       {error && <span>User not found</span>}
-     {user && <div className="userChat">
+     {user && <div className="userChat" onClick={handleSelect}>
         <img src={user.photoURL}
-           alt="" />
+           alt={user.userName} />
            <div className="userChatInfo">
             <span>{user.userName}</span>
            </div>
